fec8c905f5a2fd475791a8bd4070ee87b1d635a7
ANDRES <anfevasa@gmail.com>
2023-07-12 18:46:03 -0500

    Scanner y sout - ejercicio:1 clase:2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("primer commit");
+        Scanner keyboardInput = new Scanner(System.in);
+        // Primer ejercicio propuesto clase 2 ( e:1 c:2)
+        String name;
+        int age;
+
+        System.out.print("Ingrese su nombre: ");
+        name = keyboardInput.next();
+        System.out.print("Ingrese su edad: ");
+        age = keyboardInput.nextInt();
+
+        System.out.println("Hola, "+name+". Tienes "+ age+" años.");
     }
 }
