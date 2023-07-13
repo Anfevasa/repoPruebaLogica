e685f33dd8760cbc5efce9df69f19e06b47a9356
ANDRES <anfevasa@gmail.com>
2023-07-12 19:04:29 -0500

    Raiz cuadrada, string format, librería Math - ejercicio:2 clase:2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,25 @@ package org.example;
 
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
     public static void main(String[] args) {
         Scanner keyboardInput = new Scanner(System.in);
-        // Primer ejercicio propuesto clase 2 ( e:1 c:2)
-        String name;
+        // Segundo ejercicio propuesto - clase 2 ( e:2 c:2)
+        int numberGiven;
+        double squareRoot;
+
+        System.out.print("Ingrese un número entero: ");
+        numberGiven = keyboardInput.nextInt();
+        squareRoot = Math.sqrt((double)numberGiven);
+
+
+        System.out.println("La raíz cuadrada de "+ numberGiven+" es: "+ String.format("%.2f", squareRoot));
+
+
+        // Primer ejercicio propuesto - clase 2 ( e:1 c:2)
+        /*String name;
         int age;
 
         System.out.print("Ingrese su nombre: ");
@@ -14,6 +28,6 @@ public class Main {
         System.out.print("Ingrese su edad: ");
         age = keyboardInput.nextInt();
 
-        System.out.println("Hola, "+name+". Tienes "+ age+" años.");
+        System.out.println("Hola, "+name+". Tienes "+ age+" años.");*/
     }
 }
